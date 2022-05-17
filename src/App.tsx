18d2f03216a1eda3659ac6625878ae344aff6c48
yoichi-aoyama <aoyama.yoichi@gmail.com>
2022-05-18 00:35:42 +0900

    update: エラーハンドリング追加

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
     });
     const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        fetch(`https://api.weatherapi.com/v1/current.json?key=f7722ff8ed924c44903160153220805&q=${city}&aqi=no` )
+        fetch(`https://api.weatherapi.com/v1/current.json?key=f7722ff8ed924c44903160153220805&q=${city}&aqi=no`)
             .then(res => res.json())
             .then(data => {
                 setResults({
@@ -34,12 +34,13 @@ function App() {
                     icon: data.current.condition.icon
                 })
             })
+            .catch(err => alert("エラーが発生しました。ページをリロードしてください。"))
     }
     return (
         <div className="App">
-            <Title />
-            <Form setCity={setCity} getWeather={getWeather} />
-            <Results results={results} />
+            <Title/>
+            <Form setCity={setCity} getWeather={getWeather}/>
+            <Results results={results}/>
         </div>
     );
 }
